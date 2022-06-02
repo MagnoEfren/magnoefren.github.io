67a55940062b700e408658c273ec2d6f97a1e555
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2022-06-02 12:19:22 -0500

    Update and rename codigo.js to app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//https://riunet.upv.es/bitstream/handle/10251/179241/Lapena%20-%20Diseno%20y%20desarrollo%20de%20un%20juego%20de%20navegador%20para%20comenzar%20a%20aprender%20construcciones%20bas....pdf?sequence=1
 function saludar()
 	{
 		let saludo = prompt('COMO ESTAS?')
