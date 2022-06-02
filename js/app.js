088757fe62e0a57d7251afd94d0330325fef53ae
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2022-06-02 18:19:50 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function saludar()
+	{
+		let saludo = prompt('COMO ESTAS?')
+
+		if (saludo=='BIEN'){
+			document.write('ME ALEGRO')
+		}
+		else
+		{
+			document.write('	una pena')
+		}
+	}
+
+
+//saludar()
+
+//let dato = document.getElementsByTagName('dato')
+//alert(dato)
