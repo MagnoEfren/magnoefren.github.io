625c6895073a5a3b78fafd438c4c3d376b1623ae
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2022-06-02 12:17:58 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,19 @@
+//https://riunet.upv.es/bitstream/handle/10251/179241/Lapena%20-%20Diseno%20y%20desarrollo%20de%20un%20juego%20de%20navegador%20para%20comenzar%20a%20aprender%20construcciones%20bas....pdf?sequence=1
+function saludar()
+	{
+		let saludo = prompt('COMO ESTAS?')
+
+		if (saludo=='BIEN'){
+			document.write('ME ALEGRO')
+		}
+		else
+		{
+			document.write('	una pena')
+		}
+	}
+
+
+saludar()
+
+//let dato = document.getElementsByTagName('dato')
+//alert(dato)
