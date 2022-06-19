ce1d30e5ec429c2c8579bbf310e49b2835a511cd
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2022-06-19 01:38:12 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,58 @@
-function saludar()
-	{
-		let saludo = prompt('BIENVENIDO, COMO ESTAS?')
-
-		if (saludo=='BIEN') or (saludo == 'bien){
-			document.write('ME ALEGRO')
-		}
-		else
-		{
-			document.write('UNA PENA')
-		}
+// funcion que muestra el menu responsive
+function responsiveMenu(){
+	var x = document.getElementById("nav");
+	if(x.className===""){
+		x.className = "responsive";
 	}
 
+	else{
+		x.className ="";
+	}
+}
 
-saludar()
+// funcion que me ampli ael estil a la opcion selecionada en el menu quita la previsuacion selecionada
 
-//let dato = document.getElementsByTagName('dato')
-//alert(dato)
+function seleccionar(link){
+	var opciones= document.querySelectorAll("#links a");
+	opciones[0].className = "";
+	opciones[1].className = "";
+	opciones[2].className = "";
+	opciones[3].className = "";
+	opciones[4].className = "";
+	link.className ="seleccionado";
+	var x =  document.getElementById("nav");
+	x.className ="";
+
+}
+
+
+//Modo oscuro (Dark Mode)
+
+
+var bdark = document.querySelector("#bdark");
+var body = document.querySelector("body")
+
+load();
+
+
+bdark.addEventListener('click', e =>{
+	body.classList.toggle('darkmode');
+	store(body.classList.contains('darkmode'));
+});
+
+
+
+ function load(){
+ 	var darkmode = localStorage.getItem('darkmode');
+
+ 	if(!darkmode){
+ 		store('false');
+ 	}
+ 	else if(darkmode=='true'){
+ 		body.classList.add('darkmode');
+ 	}
+ }
+
+ function store(value){
+ 	localStorage.setItem('darkmode', value);
+ }
